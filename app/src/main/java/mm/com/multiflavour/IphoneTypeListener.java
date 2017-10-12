56ebbe5f280d9ea586b1b3732b1b8e96fb551b15
Aswini kumar rath <archu.aswini@gmail.com>
2017-10-12 15:51:02 +0530

    Sample flaviour

---
+++ app/src/main/java/mm/com/multiflavour/IphoneTypeListener.java
@@ -0,0 +1,9 @@
+package mm.com.multiflavour;
+
+/**
+ * Created by aswrath on 10/12/2017.
+ */
+
+public interface IphoneTypeListener {
+    String getphoneType();
+}
---
+++ app/src/oppo/mm/com/multiflavour/android/PhoneType.java
@@ -0,0 +1,17 @@
+package mm.com.multiflavour;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PhoneType implements IphoneTypeListener{
+    @override
+    public static string getPhonetype() {
+        return "oppo";
+    }
+}
